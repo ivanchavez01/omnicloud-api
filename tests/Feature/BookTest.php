15996543104008bbc08f5b29836a8a4f1640531e
ichavezrg <ichavez@redgirasol.com>
2022-11-06 14:45:03 -0700

    feat: feature test

--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -7,6 +7,8 @@ use App\Models\Editorial;
 use App\Models\File;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class BookTest extends TestCase
@@ -24,5 +26,26 @@ class BookTest extends TestCase
         $this->assertInstanceOf(Editorial::class, $book->editorial);
         $this->assertInstanceOf(File::class, $book->pdfFile);
         $this->assertCount(1, $book->authors->toArray());
+    }
+
+    public function testAnUserCanCreateABookViaApi(): void
+    {
+        $email = 'ichavez9001@gmail.com';
+        $password = '123456789';
+        Storage::fake('avatars');
+
+        $file = UploadedFile::fake()->image('cover.jpg');
+        $response = $this->withHeaders([
+            'Authorization' => 'Basic '. base64_encode("{$email}:{$password}")
+        ])->post('/api/book', [
+            'editorial_id' => 1,
+            'authors' => [1],
+            'pdf_file' => $file,
+            'title' => 'Fundamentals of Software Architecture',
+            'published_at' => '2022-01-19',
+            'price' => 1200,
+        ]);
+
+        $response->assertOk();
     }
 }
